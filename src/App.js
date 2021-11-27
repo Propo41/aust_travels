cdc530e02461fd98bcbf42b80c6173d6e22c105e
Propo41 <aliahnaf327@gmail.com>
2021-11-27 18:43:39 +0600

    added firebase sdk and created a context object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Router from "./routes";
 import GlobalStyles from "./theme/globalStyles";
+import firebaseApp from "auth";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { AuthProvider } from "auth/AuthContext";
+
 const { default: ThemeConfig } = require("./theme");
 
 function App() {
+  // const db = getDatabase(app);
+  // const starCountRef = ref(db, "users");
+  // onValue(starCountRef, (snapshot) => {
+  //   const data = snapshot.val();
+  //   console.log(data);
+  // });
+
   return (
     <ThemeConfig>
       <GlobalStyles />
-      <Router />
+      {/* injecting the context into the Router component so 
+      that all child components can access firebaseApp */}
+      <AuthProvider value={firebaseApp}>
+        <Router />
+      </AuthProvider>
     </ThemeConfig>
   );
 }

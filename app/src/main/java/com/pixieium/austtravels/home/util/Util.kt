7a95a095afd0c36d933cee04287333b681c03495
Propo41 <aliahnaf327@gmail.com>
2021-12-01 23:27:37 +0600

    - tried a different approach from google code labs which is the best approach. - works flawlessly.

---
+++ app/src/main/java/com/pixieium/austtravels/home/util/Util.kt
@@ -0,0 +1,48 @@
+package com.pixieium.austtravels.home.util
+
+import android.content.Context
+import android.location.Location
+import androidx.core.content.edit
+import com.pixieium.austtravels.R
+
+/**
+ * Returns the `location` object as a human readable string.
+ */
+fun Location?.toText(): String {
+    return if (this != null) {
+        "($latitude, $longitude)"
+    } else {
+        "Unknown location"
+    }
+}
+
+/**
+ * Provides access to SharedPreferences for location to Activities and Services.
+ */
+internal object SharedPreferenceUtil {
+
+    const val KEY_FOREGROUND_ENABLED = "tracking_foreground_location"
+
+    /**
+     * Returns true if requesting location updates, otherwise returns false.
+     *
+     * @param context The [Context].
+     */
+    fun getLocationTrackingPref(context: Context): Boolean =
+        context.getSharedPreferences(
+            context.getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        )
+            .getBoolean(KEY_FOREGROUND_ENABLED, false)
+
+    /**
+     * Stores the location updates state in SharedPreferences.
+     * @param requestingLocationUpdates The location updates state.
+     */
+    fun saveLocationTrackingPref(context: Context, requestingLocationUpdates: Boolean) =
+        context.getSharedPreferences(
+            context.getString(R.string.preference_file_key),
+            Context.MODE_PRIVATE
+        ).edit {
+            putBoolean(KEY_FOREGROUND_ENABLED, requestingLocationUpdates)
+        }
+}
